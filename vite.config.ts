157fab66ab5b4eb7a06617f0abe9e2978e41b724
Hamza-Dakar <mhamza@dakarsoftware.com>
2025-02-28 15:10:43 +0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cicdTest/'  // MUST match repo name
+  base: '/cicdTest/' // Ensure this matches your repo name
 });
